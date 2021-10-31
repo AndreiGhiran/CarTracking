19f50873867ed5e264b1f666c72eb58f027de470
SergiuDeveloper <sergiu.nistor@info.uaic.ro>
2021-10-31 21:32:29 +0200

    Added dispatcher logic

--- DataProcessing/Authority/src/logic/UDPEndpoint.py
+++ DataProcessing/Authority/src/logic/UDPEndpoint.py
@@ -1,4 +1,7 @@
 import socket
+import pickle
+import struct
+
 from threading import Lock
 
 from DataProcessingEntity import DataProcessingEntity
@@ -6,8 +9,7 @@ from DataProcessingEntity import DataProcessingEntity
 
 # Uses dependency injection for DataProcessingEntity
 class UDPEndpoint:
-    def __init__(self, ip: int, port: str, frame_size: int, data_processing_entity: DataProcessingEntity):
-        self.__address = (ip, port)
+    def __init__(self, frame_size: int, data_processing_entity: DataProcessingEntity):
         self.__frame_size = frame_size
         self.__can_start = True
         self.__is_running = False
@@ -19,19 +21,43 @@ class UDPEndpoint:
             raise Exception('UDPEndpoint already running.')
 
         udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_socket.bind(self.__address)
+        udp_socket.bind(('', 0))
+
+        print(udp_socket.getsockname()[1]) # Send port to Dispatcher
 
         self.__set_is_running(True)
 
         allowed_address = None
+        data = ''
+        payload_size = struct.calcsize('L')
         while self.get_is_running():
-            data, sender_address = udp_socket.recvfrom(self.__frame_size)
+            while len(data) < payload_size:
+                new_data, sender_address = udp_socket.recvfrom(min(self.__frame_size, 4096))
 
-            if allowed_address is None:
-                allowed_address = sender_address
-            elif allowed_address != sender_address:
-                print('Another client attempted to send data to the endpoint. Current sender: {}. Allowed sender: {}.'.format(sender_address, allowed_address))
-                continue
+                if allowed_address is None:
+                    allowed_address = sender_address
+                elif allowed_address != sender_address:
+                    print('Another client attempted to send data to the endpoint. Current sender: {}. Allowed sender: {}.'.format(sender_address, allowed_address))
+                    continue
+
+                data += new_data
+            packed_message_size = data[:payload_size]
+            data = data[payload_size:]
+            message_size = struct.unpack('L', packed_message_size)[0]
+
+            while len(data) < message_size:
+                new_data, sender_address = udp_socket.recvfrom(min(self.__frame_size, 4096))
+
+                if allowed_address is None:
+                    allowed_address = sender_address
+                elif allowed_address != sender_address:
+                    print('Another client attempted to send data to the endpoint. Current sender: {}. Allowed sender: {}.'.format(sender_address, allowed_address))
+                    continue
+
+                data += new_data[message_size:]
+                
+            frame = pickle.loads(data[:message_size])
+            data = data[message_size:]
 
             data_processing_entity.process_frame(frame)
 
--- DataProcessing/Authority/src/main.py
+++ DataProcessing/Authority/src/main.py
@@ -7,22 +7,19 @@ from logic.algorithms.DepthEstimation import DepthEstimation
 from logic.algorithms.ObjectDetection import ObjectDetection
 
 
-IP = '0.0.0.0'
-PORT = int(sys.argv[1])
-
-FRAME_SIZE_X = int(sys.argv[2])
-FRAME_SIZE_Y = int(sys.argv[3])
+FRAME_SIZE_X = int(sys.argv[1])
+FRAME_SIZE_Y = int(sys.argv[2])
 FRAME_SIZE = FRAME_SIZE_X * FRAME_SIZE_Y * 3
 
-LATITUDE = float(sys.argv[4])
-LONGITUDE = float(sys.argv[5])
-ROTATION_X = float(sys.argv[6])
-ROTATION_Y = float(sys.argv[7])
+LATITUDE = float(sys.argv[3])
+LONGITUDE = float(sys.argv[4])
+ROTATION_X = float(sys.argv[5])
+ROTATION_Y = float(sys.argv[6])
 
-DB_USER = sys.argv[8]
-DB_PASS = sys.argv[9]
-DB_HOST = sys.argv[10]
-DB_SCHEMA = sys.argv[11]
+DB_USER = sys.argv[7]
+DB_PASS = sys.argv[8]
+DB_HOST = sys.argv[9]
+DB_SCHEMA = sys.argv[10]
 
 
 if __name__ == "__main__":
@@ -33,5 +30,5 @@ if __name__ == "__main__":
     object_detection = ObjectDetection()
     data_processing_entity = DataProcessingEntity(LATITUDE, LONGITUDE, ROTATION_X, ROTATION_Y, depth_estimation, object_detection, database_handler)
 
-    udp_endpoint = UDPEndpoint(IP, PORT, FRAME_SIZE, data_processing_entity)
+    udp_endpoint = UDPEndpoint(FRAME_SIZE, data_processing_entity)
     udp_endpoint.run()
